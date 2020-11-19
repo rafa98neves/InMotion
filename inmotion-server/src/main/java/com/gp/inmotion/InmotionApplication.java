ad55cfd4c01e2db5d89724e010ad82b2067106aa
Catarina <catarinaql@gmail.com>
2020-11-19 19:29:19 +0000

    registo e ligação ao login

---
+++ inmotion-server/src/main/java/com/gp/inmotion/InmotionApplication.java
@@ -0,0 +1,13 @@
+package com.gp.inmotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmotionApplication.class, args);
+	}
+
+}
