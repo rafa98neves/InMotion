ff1473f451ffc36f1e4ca6dcdbb4b757ca9fa9e8
Bruno <brunocabanas@hotmail.com>
2020-12-10 10:31:00 +0000

    personal page

--- inmotion-web/src/_helpers/router.js
+++ inmotion-web/src/_helpers/router.js
@@ -2,11 +2,14 @@ import { createRouter, createWebHistory} from 'vue-router';
 import { authGuard } from './auth.guard';
 import Login from '../components/auth/Login';
 import Home from '../components/Homepage';
+import PersonalInformationPatient from '../components/auth/PersonalInformationPatient';
+import PersonalInformationTherapist from '../components/auth/PersonalInformationTherapist';
 
 const routes = [
     { path: '/', component: Home, name: 'Homepage', beforeEnter: authGuard },
     { path: '/login', component: Login, name: 'Login' },
-
+    { path: '/personalinformationpatient', component: PersonalInformationPatient, name: 'Personal Information Patient' },
+    { path: '/personalinformationtherapist', component: PersonalInformationTherapist, name: 'Personal Information Therapist' },
     // otherwise redirect to home
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
